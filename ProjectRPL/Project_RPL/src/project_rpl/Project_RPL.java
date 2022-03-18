4e1140864f02595ebcaa0084ba848ed7abe190d7
DevandaRadyaAnanta <97436671+DevandaRadyaAnanta@users.noreply.github.com>
2022-03-18 23:22:17 +0700

    Add files via upload

---
+++ ProjectRPL/Project_RPL/src/project_rpl/Project_RPL.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package project_rpl;
+
+/**
+ *
+ * @author Lucky
+ */
+public class Project_RPL {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello lucky");
+    }
+    
+}
